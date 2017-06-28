267ca16d26ca73df857f83ff131e814302669944
fmorales86mb <fmorales86mb@gmail.com>
2017-06-28 20:32:12 -0300

    Primer commit
    Subimos los dos primeros ejercicios y comentarios en el tercero.

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,13 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	//var person = prompt("Please enter your name", "Harry Potter");
+	var nombre = prompt("Por favor ingrese su nombre", "zarlanga");
+
+	// var: variable, porciòn de memoria. 
+	//alert(nombre);
+	alert("Su nombre es: "+nombre);
+	// Con el + CONCATENO.
+}
+
---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,10 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	// Prompt: pide datos. Recibe dos parametros como max. 
+	// Alert: me muestra algo.
+	
+}
+
+
