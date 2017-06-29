f98d27b2610f803e3b73f5cea0ec9057e5f5727f
fmorales86mb <fmorales86mb@gmail.com>
2017-06-28 22:07:53 -0300

    Clase 1.
    EntradaSalida -  Ejercicio 3 y 4.

--- jsEntradaSalida-3.js
+++ jsEntradaSalida-3.js
@@ -2,9 +2,23 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	// Prompt: pide datos. Recibe dos parametros como max. 
-	// Alert: me muestra algo.
+	// Prompt: pide datos. Recibe dos parametros como max. Entrada de datos!
+	// Alert: me muestra algo. Salida de datos!
+	var dato = 0;
 	
+	/*	Entrada de datos.
+
+	// todo el htm se llama documento. 
+	// el getelementbyid funciona como entrada o salida.
+	dato = document.getElementById('elNombre').value; //shift i
+	// con el value obtengo el valor.
+
+	alert (dato);
+	*/
+
+	// Muestra zarlanga en el tb.
+	document.getElementById("elNombre").value = "zarlanga";	
+
 }
 
 
