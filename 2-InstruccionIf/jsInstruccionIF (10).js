41e1c658db27b8b3476b43cbb3d6efc1df534e4e
fmorales86mb <fmorales86mb@gmail.com>
2017-07-19 11:40:45 -0300

    Instrucción If
    Ejercicios 8 y 10.

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,18 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var nota;
+
+	// round me redondea el nro.
+	// random me da un nro del [0 al 1)
+	nota = Math.round(Math.random()*10);
 	
+	if (nota >= 9){
+		alert(nota + " EXELENTE");	
+	} else if (nota >= 4){
+		alert(nota + " APROBÓ");	
+	} else {
+		alert(nota + " Vamos, la próxima se puede.");	
+	}
 
 }//FIN DE LA FUNCIÓN
